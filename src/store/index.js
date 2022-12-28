8f3524c4d34ff5328d065eedbf6c4976fb1ebb0e
TheBanusco10 <thebanusco10@gmail.com>
2022-12-28 18:34:22 +0100

    Sign in and out

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { createStore } from 'vuex'
 
+import VueCookies from "vue-cookies";
+
 export default createStore({
   state: {
+    user: VueCookies.get('midori-books') || null
   },
   getters: {
+    user: state => {
+      return state.user
+    }
   },
   mutations: {
+    setUser: (state, user) => {
+      state.user = user;
+    }
   },
   actions: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,38 @@
 </template>
 
 <script setup>
-import {GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
 
 import {auth} from "@/firebase";
 import VueCookies from "vue-cookies";
+import {useStore} from "vuex";
+
+const store = useStore();
 
 const signInWithGoogle = async () => {
+  if (store.getters.user) return;
+
   try {
     const provider = new GoogleAuthProvider();
 
     const {uid, displayName, photoURL} = (await signInWithPopup(auth, provider)).user;
-    // uid, displayName, photoURL
-    VueCookies.set('midori', {uid, displayName, photoURL}, '1d');
+
+    const userObject = {
+      uid, displayName, photoURL
+    }
+
+    VueCookies.set('midori-books', userObject, '1d');
+    store.commit('setUser', userObject);
+  }catch (err) {
+    console.error(err.message);
+  }
+}
+
+const signOutApp = async () => {
+  try {
+    await signOut(auth);
+
+    VueCookies.remove('midori-books');
   }catch (err) {
     console.error(err.message);
   }
