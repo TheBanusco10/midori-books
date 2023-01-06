b57eaddaa96b2816e76fb800f32a31d153da91c9
TheBanusco10 <thebanusco10@gmail.com>
2023-01-06 19:04:53 +0100

    HomeView cleaned

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="home">
-    <Header>
-      <template v-slot:title>
-        <p class="text-center text-2xl -mt-3">Books</p>
-      </template>
-    </Header>
   </div>
 </template>
 
 <script setup>
-
-import Header from "@/components/tags/Header.vue";
 </script>
