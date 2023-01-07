d009261b0a4bf19f7ef3b6a6378405dfc8373aeb
TheBanusco10 <thebanusco10@gmail.com>
2023-01-07 13:57:44 +0100

    Icons and PWA configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Midori books',
+    themeColor: '#059669',
+    msTileColor: '#000000',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    },
+    manifestOptions: {
+      start_url: '/'
+    }
+  },
 })
