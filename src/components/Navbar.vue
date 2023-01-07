8706524db312104f63560dcd1e4df927223ec67d
TheBanusco10 <thebanusco10@gmail.com>
2023-01-07 13:58:02 +0100

    Link to home in logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <template>
   <nav class="flex flex-wrap justify-between items-center px-3 pt-3 bg-white shadow">
-    <h1 class="w-2/12">Logo</h1>
+    <div class="w-2/12">
+      <router-link to="/">
+        <img class="w-40 pb-2 ml-5 scale-150 md:scale-100 md:ml-0"
+             :src="require('@/assets/img/logo-no-bg.svg')"
+             alt="Midori books logo">
+      </router-link>
+    </div>
     <Avatar v-if="user"/>
     <button class="border-0 border-b-2 border-gray-300 p-2 mr-2 hover:border-emerald-500 transition duration-300"
             v-else
