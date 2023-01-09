59c8f3e01577c635cfec0dce86e2b52ddafb23ad
TheBanusco10 <thebanusco10@gmail.com>
2023-01-09 12:33:18 +0100

    Version updated in package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,18 @@ module.exports = function(grunt) {
     grunt.initConfig({
         replace: {
             app_version_patch: {
-                src: ['.env', '.env.example'],             // source files array (supports minimatch)
-                overwrite: true,             // destination directory or file
+                src: ['.env', '.env.example', 'package.json'],
+                overwrite: true,
                 replacements: [{
-                    from: /'\d+\.\d+\.\d+'/g,      // regex replacement ('Fooo' to 'Mooo')
+                    from: /VUE_APP_VERSION='\d+.\d+.\d+'|"version": "\d+.\d+.\d+"/g,
                     to: patchUpdate
                 }]
             },
             app_version_minor: {
-                src: ['.env', '.env.example'],             // source files array (supports minimatch)
-                overwrite: true,             // destination directory or file
+                src: ['.env', '.env.example', 'package.json'],
+                overwrite: true,
                 replacements: [{
-                    from: /'\d+\.\d+\.\d+'/g,      // regex replacement ('Fooo' to 'Mooo')
+                    from: /VUE_APP_VERSION='\d+.\d+.\d+'|"version": "\d+.\d+.\d+"/g,
                     to: minorUpdate
                 }]
             }
@@ -36,13 +36,17 @@ module.exports = function(grunt) {
     });
 };
 
-// TODO: Change package.json too?
 const patchUpdate = matchedWord => {
-    matchedWord = matchedWord.replaceAll('\'', '');
-    const numberToUpdate = parseInt(matchedWord.substring(4));
+    console.log(matchedWord)
+    const {replaceQuotes, fullVersion, versionStartPosition} = getVersionInformation(matchedWord);
+
+    const numberToUpdate = parseInt(fullVersion.substring(4));
+
     const numberUpdated = numberToUpdate + 1;
 
-    const finalVersion = `'${replaceBetween(matchedWord, numberUpdated, 4, matchedWord.length)}'`;
+    const fullVersionUpdated = `${replaceQuotes}${replaceBetween(fullVersion, numberUpdated, 4, fullVersion.length)}${replaceQuotes}`;
+
+    const finalVersion = `${replaceBetween(matchedWord, fullVersionUpdated, versionStartPosition, matchedWord.length)}`;
 
     console.log(`New version set to ${finalVersion}`);
 
@@ -50,13 +54,45 @@ const patchUpdate = matchedWord => {
 }
 
 const minorUpdate = matchedWord => {
-    matchedWord = matchedWord.replaceAll('\'', '');
-    const numberToUpdate = parseInt(matchedWord.substring(2, 3));
+    const {replaceQuotes, fullVersion, versionStartPosition} = getVersionInformation(matchedWord);
+
+    const numberToUpdate = parseInt(fullVersion.substring(2, 3));
+
     const numberUpdated = numberToUpdate + 1;
-    const minorVersion = replaceBetween(matchedWord, numberUpdated, 2, 3);
-    const finalVersion = `'${replaceBetween(minorVersion, '0', 4, minorVersion.length)}'`
+
+    let fullVersionUpdated = `${replaceQuotes}${replaceBetween(fullVersion, numberUpdated, 2, 3)}${replaceQuotes}`;
+    fullVersionUpdated = `${replaceBetween(fullVersionUpdated, '0', 5, fullVersionUpdated.length)}`;
+
+    const finalVersion = `${replaceBetween(matchedWord, fullVersionUpdated, versionStartPosition, matchedWord.length)}${replaceQuotes}`;
 
     console.log(`New version set to ${finalVersion}`);
 
     return finalVersion;
+}
+
+/**
+ * Returns some information about the version string
+ *
+ * @param matchedWord
+ * @returns {{fullVersion: string, replaceQuotes: string, versionStartPosition: *}}
+ */
+const getVersionInformation = matchedWord => {
+    let replaceQuotes = '\'';
+    let versionStartPosition = matchedWord.indexOf('=\'');
+
+    // If it's package.json
+    if (versionStartPosition === -1) {
+        versionStartPosition = matchedWord.indexOf(' \"');
+        replaceQuotes = '"';
+    }
+
+    versionStartPosition += 1;
+
+    const fullVersion = matchedWord.substring(versionStartPosition).replaceAll(replaceQuotes, '');
+
+    return {
+        versionStartPosition,
+        replaceQuotes,
+        fullVersion
+    }
 }
