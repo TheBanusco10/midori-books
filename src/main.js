ed15f2e8a9c7a4a82dd9ca6265837357781adb31
TheBanusco10 <thebanusco10@gmail.com>
2023-01-14 13:56:16 +0100

    Scroll to top in some routes

--- src/main.js
+++ src/main.js
@@ -4,4 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(store).use(router).mount('#app');
+
+app.config.globalProperties.$scrollToTop = () => window.scrollTo(0,0);
--- src/views/books/BooksView.vue
+++ src/views/books/BooksView.vue
@@ -6,7 +6,7 @@
                 :total-items="toRaw(books)"
                 @results="getResults"
      />
-     <button @click="router.push('/books/add')"
+     <button @click="router.push('/books/add'); scrollToTop"
              class="bg-emerald-600 shadow-lg rounded-full p-3 fixed bottom-4 right-4 md:relative md:inset-0 hover:bg-emerald-500 transition duration-300"
      >
        <PlusIcon class="w-5 h-5 text-white"/>
@@ -55,7 +55,7 @@
 <script setup>
 import {query, getDocs, collection, where, doc, deleteDoc, getDoc} from "firebase/firestore";
 import {useStore} from "vuex";
-import {computed, onMounted, ref, toRaw} from "vue";
+import {computed, getCurrentInstance, onMounted, ref, toRaw} from "vue";
 
 import Container from "@/components/tags/Container.vue";
 import {db} from "@/firebase";
@@ -81,6 +81,8 @@ const booksSearched = ref([]);
 const loadingBooks = ref(true);
 const loadingAction = ref(false);
 const searching = ref(false);
+
+const scrollToTop = getCurrentInstance().appContext.config.globalProperties.$scrollToTop;
 
 const getBooks = async () => {
   try {
@@ -178,6 +180,7 @@ const onRemoveBook = async (bookID) => {
 
 const onEditBook = bookID => {
   router.push({ name: 'editBook', params: { id: bookID } });
+  scrollToTop();
 }
 
 onMounted(() => {
